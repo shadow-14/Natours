8ea4cb55a3ba267376c0e2095308808797f80a07
shadow-14 <sarthakgoyal809@gmailcom>
2024-09-18 09:44:45 +0530

    Improved stripe and add alert for succesful booking

--- controller/bookingController.js
+++ controller/bookingController.js
@@ -47,7 +47,9 @@ res.status(200).json({
 
 exports.createBookingCheckout = catchAsync(async(req,res,next)=>{
     const{tour,user,price}=req.query;
+    price = parseInt(price,10);
     if(!tour && !user && !price) return next();
+
 await Booking.create({tour,user,price});
 
 res.redirect(req.originalUrl.split('?')[0]);
