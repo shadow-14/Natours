a2e5109a52c236843bedf594f369d8bc8bfd7267
shadow-14 <sarthakgoyal809@gmailcom>
2024-09-18 08:53:28 +0530

    Improved stripe and add alert for succesful booking

--- public/js/index.js
+++ public/js/index.js
@@ -93,4 +93,4 @@ if(bookBtn){
 
 
 const alertMessage = document.querySelector('body').dataset.alert;
-if(alert)showAlert('success',alertMessage,20);
+if(alertMessage)showAlert('success',alertMessage,20);
