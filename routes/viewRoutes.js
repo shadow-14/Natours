179a9cde02e7688cd240667fad9c17a87f50019b
shadow-14 <sarthakgoyal809@gmailcom>
2024-09-18 09:49:37 +0530

    Improved stripe and add alert for succesful booking

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -7,12 +7,12 @@ const router = express.Router();
 // router.use(authController.isLoggedIn)
 // router.get('/',viewController.base)
 
-  router.get('/',authController.isLoggedIn,viewController.getOverview)
+  router.get('/',authController.isLoggedIn,bookingController.createBookingCheckout,viewController.getOverview)
   
   router.get('/tour/:slug',authController.isLoggedIn,viewController.getTour)
 
   router.get('/me',authController.protect,viewController.getAccount)
-  router.get('/my-tour',bookingController.createBookingCheckout,authController.protect,viewController.getMyTours)
+  router.get('/my-tour',authController.protect,viewController.getMyTours)
   router.get('/login',authController.isLoggedIn,viewController.getLoginForm)
   router.get('/signup',viewController.getSignUp)
   
